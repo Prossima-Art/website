0c2fee7527b7e4e8011a834c3b24d4ae8fc24200
horberlan <horberlan@gmail.com>
2021-12-09 15:48:38 -0300

    updade: firebase

--- src/server/index.js
+++ src/server/index.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 import 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyC_I2thOUwAq0zSxBVVDP91GDjnj6rQyuw",
-  authDomain: "laneartdesign-3b5c0.firebaseapp.com",
-  projectId: "laneartdesign-3b5c0",
-  storageBucket: "laneartdesign-3b5c0.appspot.com",
-  messagingSenderId: "638606377113",
-  appId: "1:638606377113:web:9f8feb1c9c8905d1fb3808",
-  measurementId: "G-J34B3DWN0R"
+  
 };
 const app = firebase.initializeApp(firebaseConfig)
 const db = getFirestore(app)
